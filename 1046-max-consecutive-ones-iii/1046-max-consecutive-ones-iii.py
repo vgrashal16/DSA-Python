1e91b1cda2dda96c2d5a28f48d7656dd8a0f2919
Grashal Vishwa <89205098+vgrashal16@users.noreply.github.com>
2026-01-22 18:58:59 +0530

    1046-MAX-CONSECUTIVE-ONES-III Stats: Time: 39 ms (95.11%), Space: 22.2 MB (23.56%) - LeetHub

--- 1046-max-consecutive-ones-iii/1046-max-consecutive-ones-iii.py
+++ 1046-max-consecutive-ones-iii/1046-max-consecutive-ones-iii.py
@@ -1,23 +1,41 @@
 class Solution:
     def longestOnes(self, arr: List[int], k: int) -> int:
-        count = {0:0, 1:0}
+        seen = 0
         window_start = 0
-        max_length = 0
-
         if 0 not in arr:
             return len(arr)
 
         for window_end in range(len(arr)):
-
             char = arr[window_end]
-            count[char] += 1
 
-            while count[0] > k:
+            if char == 0:
+                seen += 1
+            
+            if seen > k:
                 window_start_char = arr[window_start]
-                count[window_start_char] -= 1
-
+                if window_start_char == 0:
+                    seen -= 1
                 window_start += 1
+            
+        return window_end - window_start + 1
+        # count = {0:0, 1:0}
+        # window_start = 0
+        # max_length = 0
+
+        # if 0 not in arr:
+        #     return len(arr)
+
+        # for window_end in range(len(arr)):
+
+        #     char = arr[window_end]
+        #     count[char] += 1
+
+        #     while count[0] > k:
+        #         window_start_char = arr[window_start]
+        #         count[window_start_char] -= 1
+
+        #         window_start += 1
                 
-            current_length = window_end - window_start + 1
-            max_length = max(current_length, max_length)
-        return max_length
+        #     current_length = window_end - window_start + 1
+        #     max_length = max(current_length, max_length)
+        # return max_length
