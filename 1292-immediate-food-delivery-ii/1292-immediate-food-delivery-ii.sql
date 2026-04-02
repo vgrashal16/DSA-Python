42ea606136190fee0fd21ee398f5d46ca8594e33
Grashal Vishwa <89205098+vgrashal16@users.noreply.github.com>
2026-04-02 18:14:48 +0530

    1292-IMMEDIATE-FOOD-DELIVERY-II Stats: Time: 630 ms (39.86%), Space: 0B (100%) - LeetHub

--- 1292-immediate-food-delivery-ii/1292-immediate-food-delivery-ii.sql
+++ 1292-immediate-food-delivery-ii/1292-immediate-food-delivery-ii.sql
@@ -1,24 +1,37 @@
 # Write your MySQL query statement below
 
+-- select 
+
+-- round(count(case when type = "immediate" then 1 else NULL end)
+-- /
+-- count(*)
+-- *
+-- 100.00, 2)
+
+-- as immediate_percentage from (
+
+-- select *,
+-- (case when order_date = customer_pref_delivery_date
+-- then "immediate"
+-- else "scheduled"
+-- end) as type
+-- from (
+-- select *,
+-- row_number() over (partition by customer_id order by order_date asc) as rnk
+-- from Delivery) as rnk_table where rnk = 1
+
+-- ) as s 
+
 select 
 
-round(count(case when type = "immediate" then 1 else NULL end)
+round(count(case when order_date = customer_pref_delivery_date then 1 else NULL end)
 /
 count(*)
 *
 100.00, 2)
 
 as immediate_percentage from (
-
-select *,
-(case when order_date = customer_pref_delivery_date
-then "immediate"
-else "scheduled"
-end) as type
-from (
 select *,
 row_number() over (partition by customer_id order by order_date asc) as rnk
 from Delivery) as rnk_table where rnk = 1
-
-) as s 
 
